7e74f5b9af47fd99b45550e69ee822189b5cd870
RoosveltK <roosvelt.kenne.9817@facsciences-uy1.cm>
2021-08-25 20:25:46 +0100

    landing pending

--- src/app/shared/services/data.ts
+++ src/app/shared/services/data.ts
@@ -1,10 +1,19 @@
 const serviceData = [
-    { icon: 'grid', title: 'Bootstrap UI based', text: 'To an English person, it will seem like English as skeptical.' },
-    { icon: 'edit', title: 'Easy to customize', text: 'If several languages coalesce, the grammar of the language.' },
-    { icon: 'headphones', title: 'Awesome Support', text: 'The languages only differ in their grammar their pronunciation' },
-    { icon: 'layers', title: 'Creative Design', text: 'Everyone realizes why a new common would be desirable.' },
-    { icon: 'code', title: 'Quality Code', text: 'To achieve this, it would be necessary to have uniform.' },
-    { icon: 'tablet', title: 'Responsive layout', text: 'Their separate existence is a myth. For science, music, etc.' },
+  {
+    icon: "grid",
+    title: "Get best discounts",
+    text: "To an English person, it will seem like English as skeptical.",
+  },
+  {
+    icon: "grid",
+    title: "Get gifts and rewards",
+    text: "If several languages coalesce, the grammar of the language.",
+  },
+  {
+    icon: "grid",
+    title: "Get fidelity cards",
+    text: "The languages only differ in their grammar their pronunciation",
+  },
 ];
 
 export { serviceData };
