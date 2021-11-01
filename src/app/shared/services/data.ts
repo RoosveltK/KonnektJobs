9a8cead132b9f888ac97e539fa7a6ddbca66b506
RoosveltK <roosvelt.kenne.9817@facsciences-uy1.cm>
2021-11-01 16:06:22 +0100

    convert image to code

--- src/app/shared/services/data.ts
+++ src/app/shared/services/data.ts
@@ -1,18 +1,18 @@
 const serviceData = [
   {
     icon: "Percent",
-    title: `'SERVICES.service1'`,
-    text: `'SERVICES.service1.arg'`,
+    title: "SERVICES.service1",
+    text: "SERVICES.service1.arg",
   },
   {
     icon: "Gift",
-    title: `'SERVICES.service2'`,
-    text: `'SERVICES.service2.arg'`,
+    title: "SERVICES.service2",
+    text: "SERVICES.service2.arg",
   },
   {
     icon: "credit-card",
-    title: `'SERVICES.service3'`,
-    text: `'SERVICES.service3.arg'`,
+    title: "SERVICES.service3",
+    text: "SERVICES.service3.arg",
   },
 ];
 
